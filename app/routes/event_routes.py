36828a7e63b2b37acec3e6d06ab0ee531f522415
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-30 14:33:59 +0530

    post method route created

--- app/routes/event_routes.py
+++ app/routes/event_routes.py
@@ -3,3 +3,8 @@ from app.middleware import roles_required
 from app.controllers import event_controller as ctrl
 
 event_bp = Blueprint("events", __name__, url_prefix=("/api/events"))
+
+
+@event_bp.route("", methods=["POST"])
+def create_event():
+    return ctrl.create_event()
