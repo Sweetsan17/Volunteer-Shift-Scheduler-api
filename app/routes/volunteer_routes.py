65b7b4f2df51884c2829946d8b288ccf1e77e272
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-30 14:04:55 +0530

    inside of volunteer routes  post method route created

--- app/routes/volunteer_routes.py
+++ app/routes/volunteer_routes.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
 from app.middleware import roles_required
 from app.controllers import volunteer_controller as ctrl
+
+volunteer_bp = Blueprint("volunteers", __name__, url_prefix=("/api/volunteers"))
+
+
+@volunteer_bp.route("", methods=["POST"])
+def create_volunteer():
+    return ctrl.create_volunteer()
