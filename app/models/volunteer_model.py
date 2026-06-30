09f2d5d2824fd06b89598cf4dadeaa813a7b8de8
Sweetsan <coderintheworldhack100@gmail.com>
2026-06-30 10:23:00 +0530

    volunteer model file bug is fixed

--- app/models/volunteer_model.py
+++ app/models/volunteer_model.py
@@ -1,24 +1,23 @@
 from app.extensions import db
-from app.utils import utc_now
+from app.models.user_model import User
 
 
-class volunteer(db.Model):
+class Volunteer(db.Model):
     __tablename__ = "volunteers"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    volunteer_id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    volunteer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, ForeignKey=("id", User.id))
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     age = db.Column(db.Integer, nullable=False)
     is_active = db.Column(db.Boolean, default=True)
-    
+
     def to_dict(self):
         return {
             "id": self.id,
-            "volunteer_id":self.volunteer_id,
+            "volunteer_id": self.volunteer_id,
             "full_name": self.full_name,
             "email": self.email,
             "age": self.age,
             "is_active": self.is_active,
-            
         }
