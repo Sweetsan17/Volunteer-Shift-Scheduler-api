05d1c1a010be6895a993181aaf59bacf3bda4d74
Sweetsan <coderintheworldhack100@gmail.com>
2026-07-01 08:55:46 +0530

    init.py file  code upodated

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,8 @@
-from app.routes.auth_routes import auth_bp
-from app.routes.event_routes import event_bp
-from app.routes.volunteer_routes import volunteer_bp
-
-
 def register_blueprints(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(event_bp)
-    app.register_blueprint(volunteer_bp)
+    from app.routes.auth_routes import auth_bp
+    from app.routes.event_routes import event_bp
+    from app.routes.volunteer_routes import volunteer_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+    app.register_blueprint(event_bp, url_prefix="/api/events")
+    app.register_blueprint(volunteer_bp, url_prefix="/api/volunteers")
