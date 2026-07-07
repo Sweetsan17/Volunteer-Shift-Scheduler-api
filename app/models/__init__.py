22c7cb9c0f5061d403c49929f0b095342a663420
Vathumy Vaitheesvaran <vathumyv@gmail.com>
2026-07-07 11:45:35 +0530

    app/model/init.py finished

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user_model import User
 from app.models.event_model import Event
-from app.models.volunteer_model import Volunteer
+from app.models.shift_model import Shift
+from app.models.signup_model import Signup
 
-__all__ = ["User", "Event", "Volunteer"]
+__all__ = ["User", "Event", "Shift", "Signup"]
