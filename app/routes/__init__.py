76acc049bdc3b45e94a5bd11c83ad74f106ef94e
Vathumy Vaitheesvaran <vathumyv@gmail.com>
2026-07-07 11:46:13 +0530

    app/routes completed

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 def register_blueprints(app):
     from app.routes.auth_routes import auth_bp
     from app.routes.event_routes import event_bp
-    from app.routes.volunteer_routes import volunteer_bp
+    from app.routes.shift_routes import shift_bp
+    from app.routes.user_routes import user_bp
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(event_bp, url_prefix="/api/events")
-    app.register_blueprint(volunteer_bp, url_prefix="/api/volunteers")
+    app.register_blueprint(shift_bp, url_prefix="/api/shifts")
+    app.register_blueprint(user_bp, url_prefix="/api/users")
